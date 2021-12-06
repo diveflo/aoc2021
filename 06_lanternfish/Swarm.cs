d2539341136694712d9382d456c895ff94b12533
Florian Gabsteiger <florian.gabsteiger@methodpark.de>
2021-12-06 16:18:14 +0100

    ugly because of second part of the exercise for day 6 ¯\_(ツ)_/¯

---
+++ 06_lanternfish/Swarm.cs
@@ -0,0 +1,53 @@
+﻿namespace Lanternfish
+{
+    public class Swarm
+    {
+        private readonly IDictionary<uint, long> myDaysUntilReproductionAndFishCount;
+
+        public Swarm(IList<uint> initialDaysUntilReptroduction)
+        {
+            myDaysUntilReproductionAndFishCount = new Dictionary<uint, long>();
+            InitializeDaysUntilReproductionAndFishCount();
+
+            foreach (var daysUntilReproduction in initialDaysUntilReptroduction)
+            {
+                myDaysUntilReproductionAndFishCount[daysUntilReproduction] = myDaysUntilReproductionAndFishCount[daysUntilReproduction] + 1;
+            }
+        }
+
+        public void ProcessNewDay()
+        {
+            var numberOfNewFish = myDaysUntilReproductionAndFishCount[0];
+
+            myDaysUntilReproductionAndFishCount[0] = myDaysUntilReproductionAndFishCount[1];
+            myDaysUntilReproductionAndFishCount[1] = myDaysUntilReproductionAndFishCount[2];
+            myDaysUntilReproductionAndFishCount[2] = myDaysUntilReproductionAndFishCount[3];
+            myDaysUntilReproductionAndFishCount[3] = myDaysUntilReproductionAndFishCount[4];
+            myDaysUntilReproductionAndFishCount[4] = myDaysUntilReproductionAndFishCount[5];
+            myDaysUntilReproductionAndFishCount[5] = myDaysUntilReproductionAndFishCount[6];
+            myDaysUntilReproductionAndFishCount[6] = myDaysUntilReproductionAndFishCount[7] + numberOfNewFish;
+            myDaysUntilReproductionAndFishCount[7] = myDaysUntilReproductionAndFishCount[8];
+            myDaysUntilReproductionAndFishCount[8] = numberOfNewFish;
+        }
+
+        public long NumberOfFish()
+        {
+            long numberOfFish = 0;
+
+            foreach(var daysUntilReproductionAndFishCount in myDaysUntilReproductionAndFishCount)
+            {
+                numberOfFish += daysUntilReproductionAndFishCount.Value;
+            }
+
+            return numberOfFish;
+        }
+
+        private void InitializeDaysUntilReproductionAndFishCount()
+        {
+            for (uint i = 0; i < 9; i++)
+            {
+                myDaysUntilReproductionAndFishCount[i] = 0;
+            }
+        }
+    }
+}
