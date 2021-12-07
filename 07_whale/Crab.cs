1d75f7c0e64a2ecc82b256b0be8587f2800445f9
Florian Gabsteiger <florian.gabsteiger@methodpark.de>
2021-12-07 10:28:30 +0100

    day 7 🚀

---
+++ 07_whale/Crab.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Whale
+{
+    public class Crab
+    {
+        public long Position { get; set; }
+
+        public Crab(long position)
+        {
+            Position = position;
+        }
+
+        public long FuelRequiredToMoveToPosition(long position, IDictionary<long, long> distanceToFuel)
+        {
+            var distance = Math.Abs(Position - position);
+            return distanceToFuel[distance];
+        }
+    }
+}
---
+++ 07_whale/Swarm.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Whale
+{
+    public class Swarm
+    {
+        private IList<Crab> myCrabs;
+
+        public Swarm(IList<long> initialPositions)
+        {
+            myCrabs = new List<Crab>();
+
+            foreach(var position in initialPositions)
+            {
+                var crab = new Crab(position);
+                myCrabs.Add(crab);
+            }
+        }
+
+        public long FuelRequiredToMoveAllCrabsToPosition(long position, IDictionary<long, long> distanceToFuel)
+        {
+            long fuel = 0;
+
+            foreach(var crab in myCrabs)
+            {
+                fuel += crab.FuelRequiredToMoveToPosition(position, distanceToFuel);
+            }
+
+            return fuel;
+        }
+
+        public long MinimalFuelCost()
+        {
+            var minimumPosition = myCrabs.Min(x => x.Position);
+            var maximumPosition = myCrabs.Max(x => x.Position);
+
+            var distanceToFuel = DistanceToFuelPart1(maximumPosition);
+
+            long minimumFuelPosition = -1;
+            long minimumFuel = long.MaxValue;
+
+            for (var position = minimumPosition; position < maximumPosition + 1; position++)
+            {
+                var totalFuelRequired = FuelRequiredToMoveAllCrabsToPosition(position, distanceToFuel);
+                if (totalFuelRequired <= minimumFuel)
+                {
+                    minimumFuelPosition = position;
+                    minimumFuel = totalFuelRequired;
+                }
+            }
+
+            return minimumFuel;
+        }
+
+        public IDictionary<long, long> DistanceToFuelPart1(long maximumDistance)
+        {
+            var distanceToFuel = new Dictionary<long, long>();
+
+            for (var position = 0; position < maximumDistance + 1; position++)
+            {
+                distanceToFuel[position] = position;
+            }
+
+            return distanceToFuel;
+        }
+
+        public IDictionary<long, long> DistanceToFuelPart2(long maximumDistance)
+        {
+            var distanceToFuel = new Dictionary<long, long>
+            {
+                [0] = 0
+            };
+
+            for (var position = 1; position < maximumDistance + 1; position++)
+            {
+                distanceToFuel[position] = distanceToFuel[position - 1] + position;
+            }
+
+            return distanceToFuel;
+        }
+    }
+}
